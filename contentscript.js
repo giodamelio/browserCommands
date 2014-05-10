1ae001f1e27241442a79d35922d387b4e133bba5
Gio d'Amelio <giodamelio@gmail.com>
2014-05-10 12:42:34 -0700

    Basic commandbox

--- contentscript.js
+++ contentscript.js
@@ -1,13 +1,50 @@
 var isCommandBarOpen = false;
 
+// Autocomplete
+var availableTags = [
+    "ActionScript",
+    "AppleScript",
+    "Asp",
+    "BASIC",
+    "C",
+    "C++",
+    "Clojure",
+    "COBOL",
+    "ColdFusion",
+    "Erlang",
+    "Fortran",
+    "Groovy",
+    "Haskell",
+    "Java",
+    "JavaScript",
+    "Lisp",
+    "Perl",
+    "PHP",
+    "Python",
+    "Ruby",
+    "Scala",
+    "Scheme"
+];
+
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.command == "toggle-command-bar") {
         // Toggle the command bar
         if (!isCommandBarOpen) {
             // Open the command bar
-            console.log("Here!")
+
+            // Create the command bar
             var command_bar = $("<input class=\"command_bar\">");
             $("body").prepend(command_bar);
+
+            // Focus on it
+            command_bar.focus();
+
+            // Setup autocomplete
+            command_bar.autocomplete({
+                source: availableTags,
+                delay: 0,
+                autoFocus: true
+            });
         } else {
             // Close the command bar
             $("input.command_bar").remove();
