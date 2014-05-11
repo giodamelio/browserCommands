2a632b7cbbe8b485aabc377cbd25ba435cf49e23
Gio d'Amelio <giodamelio@gmail.com>
2014-05-10 20:24:33 -0700

    Add options to the command bar

--- contentscript.js
+++ contentscript.js
@@ -1,5 +1,37 @@
 var isCommandBarOpen = false;
 
+// Extend jquerys autocomplete to add options at the end
+var options = [
+    {
+        label: "Add command",
+        value: "Add command",
+        option: "add_command"
+    },
+    {
+        label: "Edit command",
+        value: "Edit command",
+        option: "edit_command"
+    },
+    {
+        label: "Delete command",
+        value: "Delete command",
+        option: "delete_command"
+    }
+];
+$.widget("custom.optionscomplete", $.ui.autocomplete, {
+    _renderMenu: function(ul, items) {
+        var that = this;
+        $.each(items, function(index, item) {
+            // If we are about to add the first options, add a seperator
+            if (item.option && $(ul).children(".ui-autocomplete-category").length === 0) {
+                ul.append("<li class='ui-autocomplete-category'>Options</li>");
+            }
+
+            that._renderItemData(ul, item);
+        });
+    }
+});
+
 // Setup the command bar
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.command == "toggle-command-bar") {
@@ -15,28 +47,49 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
             command_bar.focus();
 
             // Setup autocomplete
-            command_bar.autocomplete({
+            command_bar.optionscomplete({
                 delay: 0,
                 autoFocus: true,
+                minLength: 0,
                 source: function(request, autocomplete) {
                     // Autocomplete
                     chrome.runtime.sendMessage({command: "autocomplete", text: request.term}, function(response) {
-                        autocomplete(response.map(function(value) {
+                        // Autocomplete our commands
+                        var commandsCompletions = response.map(function(value) {
                             return {
                                 ID: value.ID,
                                 value: value.name,
                                 script: value.script
                             };
-                        }));
+                        });
+
+                        // Autocomplete our options
+                        var optionsCompletions = options.filter(function(value) {
+                            return value.label.toLowerCase().indexOf(request.term.toLowerCase()) > -1;
+                        });
+
+                        // If there is no text, only show the options
+                        if (request.term === "") {
+                            autocomplete(optionsCompletions);
+                        } else {
+                            autocomplete(commandsCompletions.concat(optionsCompletions));
+                        }
                     });
-                    
                 },
                 select: function(event, ui) {
-                    // Exacute the command
-                    eval(ui.item.script);
+                    if (ui.item.option) {
+                        // Perform the option
+                        console.log(ui.item);
+                    } else {
+                        // Exacute the command
+                        eval(ui.item.script);
+                    }
                     $("input.command_bar").remove();
                 }
             });
+
+            // Open by default
+            command_bar.optionscomplete("search", "");
         } else {
             // Close the command bar
             $("input.command_bar").remove();
